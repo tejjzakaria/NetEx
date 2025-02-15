06f25d0c6e42f15b3714ce54ffde279e00e57926
Zakaria Tejjani <zakaria.tejjani@gmail.com>
2025-02-15 23:30:28 +0100

    PHP deployment

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection details
 $host = 'localhost';
-$db = 'vyn';
-$user = 'root';
-$pass = '';
+$db = 'tejjmuab_vyn';
+$user = 'tejjmuab_vyn';
+$pass = 'Sr9g1c5e@';
 
 // Create a new connection to the database
 $conn = new mysqli($host, $user, $pass, $db);
